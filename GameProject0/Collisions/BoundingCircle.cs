5bfe604c177f57bacba5a2cd3ad1f7f356e6ba6f
Christian Moden <ctmoden@ksu.edu>
2022-02-09 13:47:25 -0700

    collission helper and boundding shape classes/structs added

---
+++ GameProject0/Collisions/BoundingCircle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GameProject0.Collisions
+{
+    public struct BoundingCircle
+    {
+        /// <summary>
+        /// center of bounding circle
+        /// </summary>
+        public Vector2 Center;
+        /// <summary>
+        /// radius of bounding circle
+        /// </summary>
+        public float Radius;
+        /// <summary>
+        /// all fields in struct must be explicitly set in constructor
+        /// </summary>
+        public BoundingCircle(Vector2 center, float radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+        
+
+        public bool CollidesWith(BoundingRectangle other)
+        {
+            return CollisionHelper.Collides(this, other);
+        }
+    }
+}
